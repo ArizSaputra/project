5ccf9a7ae26a7b48c3530bf58e90cb75a14594df
ArizSaputra <as908244@gmail.com>
2023-11-28 09:30:49 +0700

    first commit

--- pengguna/fungsi_pengguna.php
+++ pengguna/fungsi_pengguna.php
@@ -1,6 +1,11 @@
 <?php
 
 $conn = mysqli_connect("localhost", "root", "", "angsur_database");
+if (!$conn) {
+    die("Koneksi Gagal: " . mysqli_connect_error());
+} else {
+    echo "Koneksi Berhasil";
+}
 
 
 function query($query)
